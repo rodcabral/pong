a80b3fbe2234d92e325c30f48d6d5b1f21be88b3
Rodrigo Cabral <rodcabral.ct@gmail.com>
2025-01-27 20:26:49 -0300

    create game loop

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@ Game game;
 int main(void) {
     init_game(&game, "Pong", 650, 650);
 
+    setup(&game);
+
     while(game.is_running) {
+        handle_input(&game);
+        update(&game);
         render(&game);
     }
 
