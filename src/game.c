6028cec9489a16a698a07ee3dc21edbc3eec2ea0
Rodrigo Cabral <rodcabral.ct@gmail.com>
2025-01-30 19:39:39 -0300

    refactor and change speed

--- src/game.c
+++ src/game.c
@@ -10,12 +10,87 @@ Object center_bar[center_bar_total];
 
 Object player, adversary, ball;
 
+int base_speed = 20;
+int ball_base_speed = 9;
+
 int ballX = 5;
-int ballY = 5;
+int ballY = 3;
+
+int check;
 
 void reset_ball() {
     ball.rect.x = WINDOW_WIDTH / 2;
     ball.rect.y = rand() % WINDOW_HEIGHT - ball.rect.h;
+
+    ballY = 3;
+    if(check) {
+        ballX = -5;
+    } else {
+        ballX = 5;
+    }
+}
+
+void handle_ball() {
+    ball.rect.x += ballX;
+    ball.rect.y += ballY;
+
+    if(ball.rect.x >= WINDOW_WIDTH - ball.rect.w) {
+        printf("Player scores!\n");
+        check = 1;
+        reset_ball();
+    }
+
+    if(ball.rect.x <= 0) {
+        printf("Adversary scores!\n");
+        check = 0;
+        reset_ball();
+    } 
+
+    if(ball.rect.y <= 0) {
+        ballY = ball_base_speed;
+    }
+
+    if(ball.rect.y >= WINDOW_HEIGHT - ball.rect.h) {
+        ballY = ball_base_speed * -1;
+    }
+}
+
+void handle_player() {
+    if((ball.rect.x <= player.rect.x + player.rect.w) && (ball.rect.y >= player.rect.y) && (ball.rect.y <= player.rect.y + player.rect.h)) {
+        ballX = ball_base_speed;
+        ballY = ball_base_speed  * -1;
+
+        if(ball.rect.y >= player.rect.y + player.rect.h / 2) {
+            ballY = ball_base_speed - 3;
+        }
+    }
+}
+
+void handle_adversary() {
+    if((ball.rect.x >= adversary.rect.x - adversary.rect.w) && (ball.rect.y >= adversary.rect.y) && (ball.rect.y <= adversary.rect.y + adversary.rect.h)) {
+        ballX = ball_base_speed * -1;
+        ballY = ball_base_speed * -1;
+
+        if(ball.rect.y >= adversary.rect.y + adversary.rect.h / 2) {
+            ballY = ball_base_speed;
+        }
+    }
+
+    if(ball.rect.y >= WINDOW_HEIGHT / 2) {
+        adversary.rect.y += base_speed;
+    }
+
+    if(ball.rect.y <= WINDOW_HEIGHT / 2) {
+        adversary.rect.y -= base_speed;
+    }
+
+    if(adversary.rect.y <= 0) {
+        adversary.rect.y = 0;
+    }
+
+    if(adversary.rect.y + adversary.rect.h > WINDOW_HEIGHT) {
+        adversary.rect.y = WINDOW_HEIGHT - adversary.rect.h;
+    }
 }
 
 bool init_game(Game* game, const char* title, int winW, int winH) {
@@ -51,7 +126,7 @@ bool init_game(Game* game, const char* title, int winW, int winH) {
     }
 
     int p_width = 8;
-    int p_height = 115;
+    int p_height = 128;
     int p_gap = 16;
 
     player = init_object(game->renderer, p_gap, (WINDOW_HEIGHT / 2) - p_height / 2, p_width, p_height);
@@ -89,7 +164,7 @@ void handle_input(Game* game) {
     const Uint8* keys = SDL_GetKeyboardState(NULL);
 
     if(keys[SDL_SCANCODE_UP]) {
-        player.rect.y -= 10;
+        player.rect.y -= base_speed;
 
         if(player.rect.y <= 0) {
             player.rect.y = 0;
@@ -97,7 +172,7 @@ void handle_input(Game* game) {
     }
 
     if(keys[SDL_SCANCODE_DOWN]) {
-        player.rect.y += 10;
+        player.rect.y += base_speed;
 
         if(player.rect.y + player.rect.h > WINDOW_HEIGHT) {
             player.rect.y = WINDOW_HEIGHT - player.rect.h;
@@ -106,60 +181,11 @@ void handle_input(Game* game) {
 }
 
 void update() {
-    ball.rect.x += ballX;
-    ball.rect.y += ballY;
+    handle_ball();
 
-    if(ball.rect.x >= WINDOW_WIDTH - ball.rect.w) {
-        printf("Player scores!\n");
-        reset_ball();
-    }
+    handle_player();
 
-    if(ball.rect.x <= 0) {
-        printf("Adversary scores!\n");
-        reset_ball();
-    }
-
-    if((ball.rect.x <= player.rect.x + player.rect.w) && (ball.rect.y >= player.rect.y) && (ball.rect.y <= player.rect.y + player.rect.h)) {
-        ballX = 10;
-        ballY = -10;
-
-        if(ball.rect.y >= player.rect.y + player.rect.h / 2) {
-            ballY = 10;
-        }
-    }
-
-    if((ball.rect.x >= adversary.rect.x - adversary.rect.w) && (ball.rect.y >= adversary.rect.y) && (ball.rect.y <= adversary.rect.y + adversary.rect.h)) {
-        ballX = -10;
-        ballY = -10;
-
-        if(ball.rect.y >= adversary.rect.y + adversary.rect.h / 2) {
-            ballY = 10;
-        }
-    }
-
-    if(ball.rect.y <= 0) {
-        ballY = 10;
-    }
-
-    if(ball.rect.y >= WINDOW_HEIGHT - ball.rect.h) {
-        ballY = -10;
-    }
-
-    if(ball.rect.y >= WINDOW_HEIGHT / 2) {
-        adversary.rect.y += 20;
-    }
-
-    if(ball.rect.y <= WINDOW_HEIGHT / 2) {
-        adversary.rect.y -= 20;
-    }
-    
-    if(adversary.rect.y <= 0) {
-        adversary.rect.y = 0;
-    }
-
-    if(adversary.rect.y + adversary.rect.h > WINDOW_HEIGHT) {
-        adversary.rect.y = WINDOW_HEIGHT - adversary.rect.h;
-    }
+    handle_adversary(); 
 }
 
 void render(Game* game) {
