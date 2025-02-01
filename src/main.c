71bdfe8f6a97574133e9571ba8097d6a1af39814
Rodrigo Cabral <rodcabral.ct@gmail.com>
2025-01-31 23:13:31 -0300

    add fps system

--- src/main.c
+++ src/main.c
@@ -5,10 +5,23 @@ Game game;
 int main(void) {
     init_game(&game, "Pong", WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    const int FPS = 60;
+    const int frame_delay = 1000 / FPS;
+
+    Uint32 frame_start;
+    int frame_time;
+
     while(game.is_running) {
+        frame_start = SDL_GetTicks();
         handle_input(&game);
-        update();
+        update(&game);
         render(&game);
+
+        frame_time = SDL_GetTicks() - frame_start;
+
+        if(frame_delay > frame_time) {
+            SDL_Delay(frame_delay - frame_time);
+        }
     }
 
     return 0;
